bd8b62b143faefd873979eb60c341677f90d5844
mthielen16 <mthielen16@gmail.com>
2017-07-19 21:12:51 +0200

    heroku push, development db to ubu

--- db/schema.rb
+++ db/schema.rb
@@ -15,28 +15,6 @@ ActiveRecord::Schema.define(version: 20170719173158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "competition_results", force: :cascade do |t|
-    t.bigint "sporter_id"
-    t.bigint "competition_id"
-    t.integer "place"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["competition_id"], name: "index_competition_results_on_competition_id"
-    t.index ["sporter_id"], name: "index_competition_results_on_sporter_id"
-  end
-
-  create_table "competitions", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "countries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "playerdats", id: false, force: :cascade, comment: "testing" do |t|
     t.integer "id"
     t.integer "comunioid"
@@ -78,16 +56,4 @@ ActiveRecord::Schema.define(version: 20170719173158) do
     t.integer "t0713"
   end
 
-  create_table "sporters", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.bigint "country_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["country_id"], name: "index_sporters_on_country_id"
-  end
-
-  add_foreign_key "competition_results", "competitions"
-  add_foreign_key "competition_results", "sporters"
-  add_foreign_key "sporters", "countries"
 end
